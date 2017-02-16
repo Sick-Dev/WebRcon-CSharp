67d75b2531e74d3f2975149c60bc6f5fed3e9ae6
Cobo <antoniocogo@gmail.com>
2017-02-16 06:34:01 +0100

    Added Test Project

---
+++ Test Core/Program.cs
@@ -0,0 +1,11 @@
+﻿using SickDev.CommandSystem;
+using WebRcon;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Reflection;
+
+class Program {
+    static void Main() {
+    }
+}
